210052658d0ecb9a582522fa1e92330f09d9e595
Redlaze <dimagold.dg53@gmail.com>
2026-07-20 18:23:24 +0300

    add course router

--- app/api/course.py
+++ app/api/course.py
@@ -1,7 +1,21 @@
-# from fastapi import APIRouter
-#
-#
-# router = APIRouter(prefix='/course', tags=['courses'])
-#
-#
-# @router.
+from fastapi import APIRouter
+from fastapi import Query
+from sqlalchemy import select
+
+from app.core.database import session_dep
+from app.models import Course
+from app.schemas.course import CourseSchema
+
+router = APIRouter(prefix='/courses', tags=['courses'])
+
+
+@router.get('/', response_model=list[CourseSchema])
+async def get_all_courses(
+    session: session_dep,
+    size: int = Query(ge=1, le=100, default=10),
+    page: int = Query(le=0, default=0)
+):
+    stmt = select(Course).limit(size).offset(page*size)
+    courses = await session.scalars(stmt)
+
+    return courses
